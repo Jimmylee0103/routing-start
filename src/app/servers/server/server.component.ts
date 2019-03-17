bc5905a626b366286aedc49c901375d90a377348
Jimmy Lee <jimmylee0103@gmail.com>
2019-03-17 17:06:15 -0500

    adding child nested routes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -14,13 +14,13 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService,
     private route: ActivatedRoute) { }
 
-  ngOnInit() {  
+  ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
       (params: Params) => {
         this.server = this.serversService.getServer(+params['id']);
-      } 
+      }
     )
   }
 
